be3f470efbec22291585f5cf953fd198bcdd9fe4
Nafisamusani53 <nafisamusani53@gmail.com>
2025-09-04 21:11:39 +0530

    solved webhook signature error

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -2,10 +2,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { capturePayment, verifySignature, sendPaymentSuccessEmail } = require("../controller/Payment")
+const { capturePayment, verifySignature, sendPaymentSuccessEmail, webhookHandler } = require("../controller/Payment")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middleware/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment",auth, isStudent, verifySignature)
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
-
+router.post("/webhook", express.raw({ type: "application/json" }), webhookHandler);
 module.exports = router
